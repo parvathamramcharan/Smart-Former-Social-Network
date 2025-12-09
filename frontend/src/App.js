61786c4bcaa4448daeef3d1499ef65b0db8520e7
parvathamramcharan <parvathamramcharan7@gmail.com>
2025-12-09 07:26:07 +0530

    created sample login page strcuture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
+import LoginPage from "./components/LoginPage";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
